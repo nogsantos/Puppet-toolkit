82d8f5ab1f1d83aec6dd6d0e97a3798a99a384af
Fabricio Nogueira <fabricio.nogueira@instruct.com.br>
2020-05-28 17:18:17 -0300

    feat(package): Created with package

--- control-repo/manifests/sample.pp
+++ control-repo/manifests/sample.pp
@@ -1,4 +1,7 @@
-node 'default' {
+# Caso o Puppet Master não encontre nenhuma declaração de node explícita para um agente, em última
+# instância pode-se criar um node simplesmente chamado default, que casará apenas para os agentes
+# que não encontraram uma definição de node.
+node default {
   package { 'vim':
     ensure => present
   }
